450ce555e0f0338aa47155dbac6c08d0377d7188
keebrunner <keebrunner@gmail.com>
2023-02-06 07:29:51 +0400

    hw4.Task1

---
+++ src/com/st1/hw/hw4/Task1.java
@@ -0,0 +1,44 @@
+package com.st1.hw.hw4;
+
+/**
+ * Задание 1
+ * Посчитать четные и нечетные цифры целого числа и вывести их на консоль.
+ * Для решения написать 2 функции, которые будут принимать введенное целое число, а возвращать количество четных цифр
+ * (вторая функция - нечетных).
+ * <p>
+ * Например: если введено число 228910, то у него 4 четные цифры (2, 2, 8, 0) и 2 нечетные (9, 1).
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int value = 1;
+        countEven(value);
+        countOdd(value);
+        System.out.println("The value contains " + countEven(value) + " even and " + countOdd(value) + " odd numbers.");
+    }
+
+    public static int countEven(int value) {
+        boolean checker = false;
+        int counter = 0;
+        for (int i = 0; i < (value > 0 ? value : value * -1); value /= 10) {
+            checker = (value % 10) % 2 == 0;
+            if (checker) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static int countOdd(int value) {
+        boolean checker = false;
+        int counter = 0;
+        for (int i = 0; i < (value > 0 ? value : value * -1); value /= 10) {
+            checker = (value % 10) % 2 == 0;
+            if (checker == false) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
